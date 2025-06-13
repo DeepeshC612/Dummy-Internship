55c9b333bc05d620f495fd15614787aa7a5aa836
Durvashi <shindedurva0312@gmail.com>
2025-06-13 16:21:00 +0530

    rmv package-lock.json

--- src/account/SignupForm.jsx
+++ src/account/SignupForm.jsx
@@ -69,6 +69,26 @@ return (
                 </div>
 
                 <div>
+                  <label className="block font-medium text-gray-700">Year</label>
+                  <Field
+                    type="year"
+                    name="year"
+                    className="mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                  />
+                  <ErrorMessage name="year" component="div" className="text-red-500 text-sm" />
+                </div>
+
+                <div>
+                  <label className="block font-medium text-gray-700">Course</label>
+                  <Field
+                    type="Course"
+                    name="Course"
+                    className="mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                  />
+                  <ErrorMessage name="Course" component="div" className="text-red-500 text-sm" />
+                </div>
+
+                <div>
                   <label className="block font-medium text-gray-700">Password</label>
                   <Field
                     type="password"
